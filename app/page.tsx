7c3038b18eae68a70911dd40f9b436d0204e3f26
Muhammad Asif <asif88383@gmail.com>
2023-06-12 21:52:55 +0500

    add changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home({ searchParams }: HomeProps) {
     manufacturer: searchParams.manufacturer || '',
     year: searchParams.year || 2022,
     fuel: searchParams.fuel || '',
-    limit: searchParams.limit || 10,
+    limit: searchParams.limit || 50,
     model: searchParams.model || '',
   })
 
